4821376063bc84d26093c69c327ee94052d9e810
Sebastian Kopf <sebastian.kopf@colorado.edu>
2017-05-10 18:57:58 -0600

    prefix scaling unit tests

--- R/si_prefix_scaling.R
+++ R/si_prefix_scaling.R
@@ -1,8 +1,12 @@
-#' get the scaling factor for a simple si unit prefix
-#' does not currently process compound units (e.g. m/s) or powers (e.g. cm2 s-1)
-#' @param unit the unit to find the suffix for (e.g. mm, kg, ms, nA, kV)
-#' @param suffix the expected suffix (e.g. m, g, s, A, V)
+# get the scaling factor for a simple si unit prefix
+# @param unit the unit to find the suffix for (e.g. mm, kg, ms, nA, kV)
+# @param suffix the expected suffix (e.g. m, g, s, A, V)
+# @note does not currently process compound units (e.g. m/s) or powers (e.g. cm2 s-1)
 get_si_prefix_scaling <- function(unit, suffix) {
+  #safety checks
+  if(missing(unit)) stop("no unit supplied", call. = FALSE)
+  if(missing(suffix)) stop("no unit suffix specified", call. = FALSE)
+  
   # supported prefixes
   prefix <- c(f = 1e-15, p = 1e-12, n = 1e-9, "\U00B5" = 1e-6, m = 1e-3, 1,
               k = 1e3, M = 1e6, G = 1e9, T = 1e12)
@@ -12,11 +16,13 @@ get_si_prefix_scaling <- function(unit, suffix) {
   pattern <- sprintf("^(%s)%s$", prefix_pattern, suffix)
   prefixes <- unit %>% str_match(pattern) %>% { .[,2] }
   if (any(is.na(prefixes))) {
-    stop("Encountered unrecognized units: ", unit[is.na(prefixes)] %>% str_c(collapse = ", "),
-         ". Supported are for this suffix: ", prefix %>% names() %>% str_c(suffix) %>% str_c(collapse = ", "),
+    stop("Encountered unrecognized units: ", 
+         unit[is.na(prefixes)] %>% str_c(collapse = ", "),
+         ". Supported are for this suffix: ", 
+         prefix %>% names() %>% str_c(suffix) %>% str_c(collapse = ", "),
          call. = FALSE)
   }
 
   # scaling
-  prefix[sapply(prefixes, function(i) which(i == names(prefix)))]
+  prefix[sapply(prefixes, function(i) which(i == names(prefix)))] %>% unname()
 }
--- tests/testthat/test-si-prefix-scaling.R
+++ tests/testthat/test-si-prefix-scaling.R
@@ -1,5 +1,32 @@
 context("SI prefixes")
 
 test_that("common SI prefixes are supported", {
-  expect_error(get_si_prefix_scaling("lm", "m"), "unrecognized unit")
+  # parameters missing
+  expect_error(isoreader:::get_si_prefix_scaling(unit = "km"), "no unit suffix specified")
+  expect_error(isoreader:::get_si_prefix_scaling(suffix = "m"), "no unit supplied")
+  
+  # prefix mismatch
+  expect_error(isoreader:::get_si_prefix_scaling("lm", "m"), "unrecognized unit")
+  
+  # suffix mismatch
+  expect_error(isoreader:::get_si_prefix_scaling("km", "V"), "unrecognized unit") 
+  
+  # specific values
+  expect_equal(isoreader:::get_si_prefix_scaling("m", "m"), 1)
+  expect_equal(isoreader:::get_si_prefix_scaling(c("mm", "km"), "m"), c(0.001, 1000))
+  expect_equal(isoreader:::get_si_prefix_scaling(c("mm", "km"), "m"), c(0.001, 1000))
+  expect_equal(
+    isoreader:::get_si_prefix_scaling(
+      c("fm", "pm", "nm", "\U00B5m", "mm", "m", "km", "Mm", "Gm", "Tm"), "m"),
+    10^(3*c(-5:4)))
+  
+  # suffix independence
+  expect_equal(
+    isoreader:::get_si_prefix_scaling("fA", "A"), 
+    isoreader:::get_si_prefix_scaling("fV", "V"))
+  
+  # parameter order
+  expect_equal(
+    isoreader:::get_si_prefix_scaling(unit = "fA", suffix = "A"), 
+    isoreader:::get_si_prefix_scaling(suffix = "A", unit = "fA"))
 })
