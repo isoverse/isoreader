b747b5bb1b7bc3974f5e187d875fd2c5827722da
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-21 18:14:22 -0600

    package documentation

--- R/package.R
+++ R/package.R
@@ -9,5 +9,11 @@
 NULL
 
 ## quiets concerns of R CMD check about . that appears in pipelines
-utils::globalVariables(c("."))
+utils::globalVariables(c(".", "y"))
 
+# release questions 
+release_questions <- function() {
+  c(
+    "Is it passing travis, appveyor and win-builder?"
+  )
+}
