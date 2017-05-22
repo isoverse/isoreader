2e754d1fc685047d3b9a03eb421aa310becc3960
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-21 22:20:50 -0600

    message on/off documentation

--- R/defaults.R
+++ R/defaults.R
@@ -12,11 +12,15 @@ set_default <- function(name, value, overwrite = TRUE) {
   return(invisible(value))
 }
 
-#' Turn on information messages
-#' Turns on information messages in all subsequent function calls by changing the global default for the \code{quiet} parameter of most isoreader functions. This function can be called stand alone or within a pipeline to turn messages on at a certain point during the pipeline.
+#' Turn information messages on/off
+#' 
+#' These functions turn information messages on/off in all subsequent function calls by changing the global default for the \code{quiet} parameter of most isoreader functions. These functions can be called stand alone or within a pipeline to turn messages on/off at a certain point during the pipeline.
 #' 
 #' @param data a data frame - returned as is if provided (e.g. in the middle of a pipeline)
-#' @family info messages
+#' @name info_messages
+NULL
+
+#' @rdname info_messages
 #' @export
 turn_info_messages_on <- function(data) {
   set_default("quiet", FALSE)
@@ -24,11 +28,7 @@ turn_info_messages_on <- function(data) {
   if (!missing(data)) return(data)
 }
 
-#' Turn off information messages
-#' Turns off information messages in all subsequent function calls by changing the global default for the \code{quiet} parameter of most isoreader functions. This function can be called stand alone or within a pipeline to turn messages off at a certain point during the pipeline.
-#' 
-#' @param data a data frame - returned as is if provided (e.g. in the middle of a pipeline)
-#' @family info messages
+#' @rdname info_messages
 #' @export
 turn_info_messages_off <- function(data) {
   set_default("quiet", TRUE)
