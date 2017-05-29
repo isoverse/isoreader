9357523dbacedd44432e56be74280cd435db8058
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-29 14:20:21 -0600

    supported file type

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,54 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
 
+# helper to make sure columns exist
+col_check <- function(col, data, fun = sys.call(-1)) {
+  if (!is.null(col) && !col %in% names(data)) 
+    stop("column not in dataset: '", col, "'. You may have to change the default parameters in your call to ", fun, call. = FALSE)
+}
+
+# get supported dual inlet file types and which functions they map to
+get_supported_di_files <- function() {
+  tribble(
+    ~extension, ~fun,        ~description,
+    "did",      isoread_did, "Isodat Dual Inlet file format"
+  )
+}
+
+# get supported continuous flow file types and which functions they map to
+get_supported_cf_files <- function() {
+  tribble(
+    ~extension, ~fun,        ~description,
+    "cf",       isoread_cf,  "Isodat Continuous Flow file format (older)",
+    "dxf",      isoread_dxf, "Isodat Continuous Flow file format (newer)"
+  )
+}
+
+#' Show supported file types
+#' @export
+show_supported_file_types <- function() {
+  sprintf(
+    c("Isoreader supported file types",
+      "Dual Inlet | 'isoread_dual_inlet()':",
+      "   %s", 
+      "Continuous flow | 'isoread_continuous_flow()':",
+      "   %s") %>% str_c(collapse = "\n"),
+    get_supported_di_files() %>% 
+      mutate(label = str_c(".", extension, " = ", description)) %>% 
+      { str_c(.$label, collapse = "\n   ") },
+    get_supported_cf_files() %>% 
+      mutate(label = str_c(".", extension, " = ", description)) %>% 
+      { str_c(.$label, collapse = "\n   ") }
+  ) %>% 
+    cat()
+}
+
+
+# get file extension
+get_file_ext <- function(filepath) {
+  basename(filepath) %>% str_extract("\\.[^.]+$")
+}
 
 # get all files in supplied folders that fit the provided data types
 # @param extensions regular extensions (without the dots), will be turned into regexp pattern
@@ -20,14 +70,25 @@ retrieve_file_paths <- function(paths, extensions = c()) {
          "):\n\t", paths[!isdir][!has_ext] %>% str_c(collapse = "\n\t"), call. = FALSE)
   
   # retrieve all the files
-  mapply(function(path, isdir) {
-    list(
-      if (!isdir) return(path)
-      else file.path(path, list.files(path, pattern = pattern, recursive = TRUE, include.dirs = FALSE))
-    )
-  }, paths, isdir, USE.NAMES = FALSE) %>% 
+  filepaths <- 
+    mapply(function(path, isdir) {
+      list(
+        if (!isdir) return(path)
+        else file.path(path, list.files(path, pattern = pattern, recursive = TRUE, include.dirs = FALSE))
+      )
+    }, paths, isdir, USE.NAMES = FALSE) %>% 
     # simplify
     unlist() %>% 
     # make sure all files unique
     unique()
+  
+  # double check that filenames are unique
+  filenames <- basename(filepaths)
+  if (anyDuplicated(filenames)) {
+    dups <- duplicated(filenames) | duplicated(filenames, fromLast = T)
+    stop("some files from different folders have identical file names:\n\t", 
+         filepaths[dups] %>% str_c(collapse = "\n\t"), call. = FALSE)
+  }
+   
+  return(filepaths)
 }
