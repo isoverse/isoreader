728e9db8b2cfc6cbf9152bf6bd5dbeb4613d486b
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-31 12:38:53 -0600

    data structure update with subpath

--- R/isodata_structures.R
+++ R/isodata_structures.R
@@ -1,36 +1,89 @@
-# basic dual inlet data structure
-make_di_data_structure <- function() {
+# Structures ----
+
+# basic data structure
+make_iso_data_structure <- function() {
   structure(
     list(
       file_info = list(
-        file_name = NULL,
-        file_path = NULL
+        file_id = NA_character_, # unique identifer
+        file_path = NA_character_, # path to file (file extension is key for processing)
+        file_subpath = NA_character_ # sub path in case file is an archieve
       ),
-      mass_data = data_frame()
+      mass_data = data_frame() # all mass data
     ),
-    class = c("dual_inlet", "isofile")
-  )
+    class = c("isofile")
+  ) %>% 
+    initialize_problems_attribute()
+}
+
+
+# basic dual inlet data structure
+make_di_data_structure <- function() {
+  struct <- make_iso_data_structure()
+  class(struct) <- c("dual_inlet", class(struct))
+  return(struct)
 }
 
 # basic continuous flow data structure
 make_cf_data_structure <- function() {
-  structure(
-    list(
-      file_info = list(
-        file_name = NULL,
-        file_path = NULL
-      ),
-      mass_data = data_frame()
-    ),
-    class = c("continuous_flow", "isofile")
-  )
+  struct <- make_iso_data_structure()
+  class(struct) <- c("continuous_flow", class(struct))
+  return(struct)
 }
 
+# Printing ----
+
+#' Print a collection of isofiles
+#' @param x Object to show.
+#' @param ... additional parameters passed to print.default
+#' @export
+print.isofiles <- function(x, ...) {
+  sprintf("# data from %d isofiles:\n", length(x)) %>% 
+    cat()
+  sapply(x, print)
+}
+
+#' Print a dual inlet isofile
+#' @param x Object to show.
+#' @param ... additional parameters passed to print.default
+#' @export
+print.dual_inlet <- function(x, ...) {
+  sprintf("Dual inlet data '%s' (file %s%s)\n", 
+          x$file_info$file_id,
+          x$file_info$file_path,
+          x$file_info$file_subpath %>% { if(!is.na(.)) str_c("|", .) else "" }
+  ) %>% cat()
+  if (n_problems(x) > 0) {
+    cat("Encountered problems:\n")
+    print(problems(x), ...)
+    cat("\n")
+  }
+}
+
+#' Print a continuous_flow isofile
+#' @param x Object to show.
+#' @param ... additional parameters passed to print.default
+#' @export
+print.continuous_flow <- function(x, ...) {
+  sprintf("Continuous flow data '%s' (file %s%s)\n", 
+          x$file_info$file_id,
+          x$file_info$file_path,
+          x$file_info$file_subpath %>% { if(!is.na(.)) str_c("|", .) else "" }
+  ) %>% cat()
+  if (n_problems(x) > 0) {
+    cat("Encountered problems:\n")
+    print(problems(x), ...)
+    cat("\n")
+  }
+}
+
+
 # set data structure file path
-set_ds_file_path <- function(ds, file_path) {
+set_ds_file_path <- function(ds, file_path, file_id = basename(file_path), file_subpath = NA_character_) {
   if (!is(ds, "isofile")) stop("can only set path for isofile data structures", call. = FALSE)
   if (!file.exists(file_path)) stop("file/folder does not exist: ", file_path, call. = FALSE)
-  ds$file_info$file_name <- basename(file_path)
   ds$file_info$file_path <- file_path
+  ds$file_info$file_id <- file_id
+  ds$file_info$file_subpath <- file_subpath
   return(ds)
 }
--- tests/testthat/test-data-structures.R
+++ tests/testthat/test-data-structures.R
@@ -16,8 +16,18 @@ test_that("can set file path for data structures", {
   expect_error(isoreader:::set_ds_file_path(data_frame()), "can only set path for isofile data structures")
   expect_silent(ds <- isoreader:::make_cf_data_structure())
   expect_error(isoreader:::set_ds_file_path(ds, "DOESNOTEXIST"), "does not exist")
+  
+  # default
   expect_is(ds <- isoreader:::set_ds_file_path(ds, system.file("extdata", package = "isoreader")), "isofile")
   expect_equal(ds$file_info$file_path, system.file("extdata", package = "isoreader"))
-  expect_equal(ds$file_info$file_name, basename(system.file("extdata", package = "isoreader")))
+  expect_equal(ds$file_info$file_id, basename(system.file("extdata", package = "isoreader")))
+  expect_equal(ds$file_info$file_subpath, NA_character_)
+  
+  # custom id and subpath
+  expect_is(ds <- isoreader:::set_ds_file_path(ds, system.file("extdata", package = "isoreader"),
+                                               "my_id", "subpath"), "isofile")
+  expect_equal(ds$file_info$file_path, system.file("extdata", package = "isoreader"))
+  expect_equal(ds$file_info$file_id, "my_id")
+  expect_equal(ds$file_info$file_subpath, "subpath")
 })
 
