d1d8dad687f5374fc59255b64e7a5a0fde136efb
Sebastian Kopf <seb.kopf@gmail.com>
2017-06-01 08:37:23 -0600

    draft of data aggregation functions

---
+++ R/aggregate_data.R
@@ -0,0 +1,27 @@
+#' Aggregate mass data
+#' 
+#' @param isofiles isotope file objects
+#' @family data_aggregation
+#' @export
+get_mass_data <- function(isofiles) {
+  
+}
+
+#' Aggregate file info
+#'
+#' @inheritParams get_mass_data
+#' @family data_aggregation
+#' @export
+get_file_info <- function(isofiles) {
+  
+}
+
+
+#' Aggregate table data
+#' 
+#' @inheritParams get_mass_data
+#' @family data_aggregation
+#' @export
+get_data_table <- function(isofiles) {
+  
+}
