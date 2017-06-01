8ab1c1f0c71ff7e045212c4f0deb8da72c036b16
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-31 20:13:50 -0600

    flow_iarc file reader first version

--- R/isoread_flow_iarc.R
+++ R/isoread_flow_iarc.R
@@ -1,11 +1,227 @@
 # read ionos .iarc archieves for their continuous flow data
-# @param ds the data structure to fill
-isoread_flow_iarc <- function(ds) {
+# @param ds the isofile data structure to fill
+isoread_flow_iarc <- function(ds, quiet = default("quiet")) {
   
-  if(!is(ds, "isofile") || !is(ds, "dual_inlet")) 
-    stop("data structure must have class 'isofile' and 'dual_inlet'", call. = FALSE)
+  if(!is(ds, "isofile") || !is(ds, "continuous_flow")) 
+    stop("data structure must have class 'isofile' and 'continuous_flow'", call. = FALSE)
   col_check(c("file_info", "mass_data"), ds)
-  col_check(c("file_name", "file_path"), ds$file_info)
+  col_check(c("file_id", "file_path", "file_subpath"), ds$file_info)
   
-  return(ds)  
+  # unzipping iarc archieve ====
+  folder_name <- ds$file_info$file_path %>% basename() %>% { str_replace(., fixed(get_file_ext(.)), "") }
+  folder_path <- file.path(tempdir(), folder_name)
+  if (!file.exists(folder_path)) {
+    if (!quiet) message("     unpacking isoprime archieve file...")
+    unzip(ds$file_info$file_path, exdir = folder_path)
+  }
+  
+  # info file ====
+  info_file <- list.files(folder_path, pattern = "^Info$", full.names = T)
+  if (length(info_file) != 1) {
+    stop("no Info xml file found in iarc", call. = FALSE)
+  }
+  processing_lists <- process_iarc_info_xml(info_file) 
+  col_check(c("DefinitionUniqueIdentifier"), processing_lists, 
+            msg = "iarc Info processing list information insufficient")
+  
+  # methods files ====
+  method_files <- list.files(folder_path, pattern = "^Method", full.names = T)
+  if (length(method_files) == 0) {
+    stop("no Method xml file(s) found in iarc", call. = FALSE)
+  }
+  method_params <- process_iarc_methods_xml(method_files)
+  
+  # tasks files ====
+  task_files <- list.files(folder_path, pattern = "^Task", full.names = T)
+  if (length(task_files) == 0) {
+    stop("no Task xml file(s) found in iarc", call. = FALSE)
+  }
+  tasks <- process_iarc_tasks_xml(task_files, method_params) 
+  col_check(c("GlobalIdentifier", "Name", "Id", "ProcessingListTypeIdentifier"), 
+            map(tasks, "info") %>% bind_rows(), msg = "iarc tasks' information insufficient")
+  
+  # processing lists / gas configuration ====
+  all_processing_lists <- 
+    tasks %>% map("info") %>% bind_rows() %>% group_by(ProcessingListTypeIdentifier) %>% summarise(samples=n()) %>% 
+    full_join(processing_lists, by = c("ProcessingListTypeIdentifier" = "DefinitionUniqueIdentifier"))
+  
+  # safety check on processing lists (make sure all processing lists defined in tasks have a ProcessingListId)
+  if (any(is.na(all_processing_lists$ProcessingListId))) {
+    sprintf("mismatch between processing lists in tasks ('%s') and in iarc info ('%s')",
+            with(all_processing_lists, ProcessingListTypeIdentifier[is.na(samples)]) %>% str_c(collapse = "', '"),
+            with(all_processing_lists, ProcessingListTypeIdentifier[is.na(ProcessingListId)]) %>% str_c(collapse = "', '")) %>% 
+      stop(call. = FALSE)
+  }
+  
+  # get gas configurations
+  used_processing_lists <- filter(all_processing_lists, !is.na(samples))
+  gas_configs <- mapply(
+    process_iarc_processing_xml, 
+    used_processing_lists$ProcessingListTypeIdentifier, 
+    file.path(folder_path, str_c("ProcessingList_", used_processing_lists$ProcessingListId)))
+  
+  # read sample/task data ====
+  isofiles <- process_iarc_samples(ds, tasks)
+  class(isofiles) <- c("isofiles", class(isofiles))
+  
+  # propagate problems =====
+  iarc_problems <- combined_problems(
+    processing_lists, method_params, tasks, gas_configs)
+  
+  # compile all problems 
+  all_problems <- data_frame()
+  for (i in 1:length(isofiles)) {
+    isofile_problems <- get_problems(isofiles[[i]]) %>% 
+      mutate(file_id = isofiles[[i]]$file_info$file_id) 
+    all_problems <- bind_rows(all_problems, isofile_problems)
+    # add general iarc problems to individual files
+    isofiles[[i]] <- set_problems(
+      isofiles[[i]], bind_rows(iarc_problems, get_problems(isofiles[[i]])))
+  }
+  
+  all_problems <- bind_rows(
+    mutate(iarc_problems, file_id = basename(ds$file_info$file_path)),
+    all_problems
+  )
+  
+  # final isofiles
+  isofiles %>% set_problems(all_problems)
+}
+
+# process iarc samples
+process_iarc_samples <- function(isofile_template, tasks) {
+  # function to generate sample id
+  generate_task_sample_id <- function(task) {
+    str_c(task$info$Id, "_", task$info$Name)
+  }
+  
+  # sort task to process in order
+  tasks <- tasks %>% sort_by(generate_task_sample_id)
+  
+  # loop through and process info and data
+  sapply(tasks, function(task) {
+    # prepare isofile object
+    isofile <- isofile_template %>% 
+      # set file path parameters
+      set_ds_file_path(
+        file_path = isofile_template$file_info$file_path, 
+        file_id = generate_task_sample_id(task), 
+        file_subpath = task$filename)
+    
+    # processing info
+    if (!default("quiet")) {
+      sprintf("      processing sample '%s' (IRMS data %s)",
+              generate_task_sample_id(task), 
+              task$data_files %>% filter(TypeIdentifier == "Acquire") %>% 
+              {.$DataFile} %>% { if(length(.) > 0) str_c(., collapse = "', '") else "" }
+              #task$info$GlobalIdentifier
+              ) %>% 
+        message()
+    }
+    
+    # process task info
+    isofile <- exec_func_with_error_catch(process_iarc_sample_info, isofile, task)
+    
+    # process task data
+    isofile <- exec_func_with_error_catch(process_iarc_sample_data, isofile, task, 
+                                          temp_dir = folder, gas_configs = gas_configs)
+    
+    return(list(isofile) %>% setNames(isofile$file_info$file_id))
+  })
+}
+
+# process iarc task info
+# @param isofile task
+process_iarc_sample_info <- function(isofile, task, quiet = default("quiet")) {
+  isofile$file_info <- c(isofile$file_info, as.list(task$info))
+  return(isofile)
+}
+
+# process iarc task data
+# @param temp_dir the temporary directory where the files are unzipped
+# @param gas_configs the gas configurations
+process_iarc_sample_data <- function(isofile, task, temp_dir, gas_configs, quiet = default("quiet")) {
+  # aquire = IRMS data
+  irms_data <- task$data_files %>% filter(TypeIdentifier == "Acquire")
+  if (nrow(irms_data) == 0) stop("no IRMS acquisitions associated with this sample", call. = FALSE)
+  
+  # check for gas configurations
+  if (!task$info$ProcessingListTypeIdentifier %in% names(gas_configs)) 
+    stop("no gas configurations specified for sample processing list ", task$info$ProcessingListTypeIdentifier, call. = FALSE)
+  gas_config <- gas_configs[[task$info$ProcessingListTypeIdentifier]]
+  
+  # read data
+  dt_format <- "%Y-%m-%dT%H:%M:%OS" # with fractional seconds
+  for (i in 1:nrow(irms_data)) {
+    isofile <- with(irms_data[i,], {
+      filepath <- file.path(temp_dir, DataFile)
+      run_time.s <- interval(strptime(AcquireStartDate, dt_format), strptime(AcquireEndDate, dt_format) ) / duration(1, "s")
+      read_irms_data_file(isofile, filepath, gas_config, run_time.s, data_units = "nA")
+    })
+  }
+  
+  return(isofile)
+}
+
+# read irms data file and convert the scan to column format tp, time.s, iXX.[data_units] based on gas configuration
+# will also add H3 factor if part of the gas configuration
+# @param isofile
+read_irms_data_file <- function(isofile, filepath, gas_config, run_time.s, data_units = "nA") {
+  if (!"DataSet" %in% h5ls(filepath)$name)
+    stop("expected DataSet attribute not present in HDF5 data file", call. = FALSE)
+  
+  # attributes (NOTE: not sure what to do with the $Tuning information (usually not filled))
+  dataset_attributes <- h5readAttributes(filepath, "DataSet")
+  if (!dataset_attributes$Species %in% names(gas_config$species))
+    stop("gas configuration for species ", dataset_attributes$Species, " not specified", call. = FALSE)
+  config <- gas_config$species[[dataset_attributes$Species]]
+  
+  # read irms data and determine which beams are used
+  #h5read(filepath, "DataSet") %>% as_data_frame()
+  irms_data <- test_data
+  data_channels <- irms_data %>% names() %>% str_subset("^Beam")
+  config_channels <- config$channels %>% filter(channel %in% data_channels)
+  
+  # safety check for channels
+  if ( length(missing <- setdiff(data_channels, config_channels$channel)) > 0)
+    stop("no channel information in gas configuration for ", data_channels %>% str_c(collapse = ", "), call. = FALSE)
+  
+  multiple <- config_channels %>% group_by(channel) %>% summarize(n = n(), masses = str_c(mass, collapse = ", "))
+  if (any(multiple$n > 1)) {
+    stop("cannot process beam channels, some channels assigned to more than one mass: ",
+         multiple %>% filter(n > 1) %>% mutate(label = str_c(channel, ": ", masses)) %>%
+         { .$label } %>% str_c(collapse = "; "), call. = FALSE)
+  }
+  
+  # h3 factor
+  if (!is.null(config$H3_factor))
+    isofile$file_info$H3_factor <- config$H3_factor
+  
+  # rename channels
+  rename_dots <- config_channels %>% { setNames(.$channel, str_c("i", .$mass, ".", data_units)) }
+  irms_data <- irms_data %>% rename_(.dots = rename_dots)
+  
+  # scale currents
+  data_scaling <- 1/get_si_prefix_scaling(data_units, "A")
+  scale_data <- function(x) x*data_scaling
+  irms_data <- irms_data %>% mutate_at(vars(starts_with("i")), scale_data)
+  
+  # scale time
+  dt <- run_time.s / nrow(irms_data)
+  irms_data <- irms_data %>% 
+    rename(tp = Scan) %>% 
+    mutate(time.s = dt * tp) %>% 
+    select(tp, time.s, everything())
+  
+  # store mass data
+  if (nrow(isofile$mass_data) > 0) {
+    existing <- isofile$mass_data %>% select(starts_with("i")) %>% names()
+    if ( any(dups <- existing %in% names(irms_data)) )
+      stop("same ions reported in multiple data files, cannot reconcile duplicate data: ", 
+           str_c(existing[dups], collapse = ", "), call. = FALSE)
+  } else {
+    isofile$mass_data <- irms_data
+  }
+  
+  return(isofile)
 }
---
+++ R/utils_xml_files.R
@@ -0,0 +1,253 @@
+# XML file utils =============
+
+# maps nodes' children as text
+map_xml_children <- function(nodes, select = NULL) {
+  nodes %>% 
+    map_df(function(node) {
+      as_list(node) %>% 
+        # if select is specific, only take the children specific
+      { if(is.null(select)) . else .[select[select %in% names(.)]] } %>% 
+        # map all as text ignoring everything that does not have exactly 1 value
+        map_chr(function(x) if (length(x) == 1) x[[1]] else NA_character_) %>% 
+        # convert to data frame
+        as.list() %>% as_data_frame()
+    })
+}
+
+# retrieve Identifier/Value pairs from 'container' type children of current node
+xml_fetch_container_value <- function(xml, ids, container = "PersistedPropertyBagProperty") {
+  sapply(ids, function(id) {
+    xml %>% xml_find_all(str_c(".//", container, "[Identifier[.='", id, "']]")) %>% 
+      xml_child("Value") %>% xml_text() %>% list()
+  })
+}
+
+# XML iarc xml file processing ========
+
+# process iarc info xml file
+process_iarc_info_xml <- function(filepath, quiet = default("quiet")) {
+  info_xml <- read_xml(filepath, encoding = "UTF-8")
+  info_version <- info_xml %>% xml_child("Version") %>% xml_text()
+  
+  # retrieve processing lists information
+  processing_lists <- 
+    info_xml %>% xml_child("ProcessingLists") %>% 
+    xml_children() %>% 
+    map_xml_children() 
+  
+  # version safety check
+  supported_versions <- c("2")
+  if (!info_version %in% supported_versions) {
+    processing_lists <- 
+      register_warning(
+        processing_lists,
+        str_c("iarc info xml lists Version ", info_version, 
+              " which has not been tested and may cause unexpected processing issues"))
+  }
+  
+  # processing lists safety check
+  if (nrow(processing_lists) == 0) {
+    stop("no processing lists in info xml", call. = FALSE)
+  }
+  
+  # information
+  if (!quiet) {
+    sprintf("      found %d processing list(s) in .iarc: '%s'", 
+            nrow(processing_lists), 
+            str_c("ProcessingList_", processing_lists$ProcessingListId, collapse = "', '")) %>% 
+      message()
+  }
+  
+  return(processing_lists)
+}
+
+# process iarc methods xml files
+process_iarc_methods_xml <- function(filepaths, quiet = default("quiet")) {
+  
+  method_params <- 
+    filepaths %>% 
+    lapply(function(methods_file) {
+      method_xml <- read_xml(methods_file, encoding = "UTF-8")
+      # id
+      method_id <- method_xml %>% xml_child("Id") %>% xml_text()
+      # method parameters
+      method_xml %>% 
+        xml_find_all(".//SerialisedFlowParameter") %>% 
+        map_xml_children() %>% 
+        mutate(MethodId = method_id,
+               MethodFile = basename(methods_file))
+    }) %>% 
+    bind_rows()
+  
+  # info
+  if (!quiet) {
+    method_files <- method_params$MethodFile %>% unique()
+    sprintf("      found %d method(s) in .iarc: '%s'", 
+            method_files %>% length(), 
+            str_c(method_files, collapse = "', '")) %>% 
+      message()
+  }
+  
+  return(method_params)
+}
+
+# process iarc tasks xml files
+process_iarc_tasks_xml <- function(filepaths, method_parameters, quiet = default("quiet")) {
+  
+  
+  process_iarc_task_xml <- function(task_file) {
+    # read file
+    task_xml <- read_xml(task_file, encoding = "UTF-8")  
+    
+    # retrieve general task info
+    task_info <- 
+      c("GlobalIdentifier", "Name", "Id", 
+        "AcquisitionStartDate", "AcquisitionEndDate", # not sure these are useful
+        "CompletionState", "MethodId", "ProcessingListTypeIdentifier") %>% 
+      sapply(function(child) task_xml %>% xml_child(child) %>% xml_text() %>% list())
+    
+    # retrieve task values based on methods information
+    task_values <-
+      task_xml %>% 
+      xml_find_all(".//SerialisableTaskValue") %>% 
+      map_xml_children() %>% 
+      # link with parameters defined in methods
+      mutate(
+        MethodId = task_info$MethodId,
+        GlobalIdentifier = task_info$GlobalIdentifier) %>% 
+      full_join(method_parameters, by = c("MethodId" = "MethodId", "ParameterIdentifier" = "Id"))
+    
+    # @NOTE: TypeIdentifier in the method_parameters holds the data type but even for numbers it seems to always be "String", currently not processed further (i.e. not turned into a different data type)
+    
+    # retrieve task data (where the real information is recorded)
+    task_data <- 
+      task_xml %>% 
+      xml_find_all(".//SerialisableDataSet") %>% 
+      map_xml_children(sel = c("Id", "AcquireDataStatus", "AcquireStartDate", "AcquireEndDate", "TypeIdentifier")) %>% 
+      mutate(
+        GlobalIdentifier = task_info$GlobalIdentifier,
+        DataFile = str_c(Id, ".hdf5")) %>% 
+      select(-Id)
+    
+    # return
+    list(
+      filename = basename(task_file),
+      # combine task info with task values 
+      info = 
+        task_info %>% as_data_frame() %>% 
+        left_join(
+          # wide format for task values
+          task_values %>% select(GlobalIdentifier, DisplayName, Value) %>% spread(DisplayName, Value), 
+          by = "GlobalIdentifier"),
+      # task data
+      data_files = task_data
+    )
+  }
+  
+  # for all task files, run the processing function
+  tasks <- filepaths %>% lapply(process_iarc_task_xml)
+  
+  if (!quiet) {
+    sprintf("      found %d sample(s) in .iarc", length(tasks)) %>% 
+      message()
+  }
+  
+  # combine info and data_files across tasks
+  return(tasks)
+}
+
+# process iarc tasks xml files
+process_iarc_processing_xml <- function(processing_list_id, filepath, quiet = default("quiet")) {
+  if (!file.exists(filepath)) stop("invalid processing list file path: ", filepath, call. = FALSE)
+  if (!quiet) {
+    sprintf("      searching processing list '%s' for gas configurations...", basename(filepath)) %>% 
+      message()
+  }
+  
+  # read file
+  xml <- read_xml(filepath, encoding = "UTF-8")
+  global_id <- xml %>% xml_child("DefinitionUniqueIdentifier") %>% xml_text()
+  
+  # safety check
+  if (global_id != processing_list_id) {
+    sprintf("mismatch between Info processing list ID ('%s') and processing list file id ('%s')",
+            processing_list_id, global_id, call. = FALSE)
+  }
+  
+  ## helper functions ##
+  # find the species
+  xml_find_species <- function(node) {
+    # potentially useful(?): DetectionBeamChannel
+    node %>% xml_child("SerialisedPropertyBagProperties") %>%
+      xml_fetch_container_value("Species") %>% { .$Species }
+  }
+  
+  # find the channel masses from the beam ratio definitions
+  xml_find_channel_masses <- function(node) {
+    # find the beam ratio definitions
+    ratio_defs <-
+      node %>% xml_child("SerialisedChildPropertyBags") %>% 
+      xml_find_all(".//SerialisablePropertyBag[Identifier[.='{42D28191-A6E9-4B7B-8C3D-0F0037624F7D}']]") %>%  
+      map(xml_fetch_container_value, c("NumeratorBeamChannel", "DenominatorBeamChannel", "Label")) %>%
+      bind_rows() 
+    if (nrow(ratio_defs) == 0) return (data_frame(channel = character(), mass = character()))  
+    
+    # derive channel defintions
+    channel_defs <-
+      ratio_defs %>% 
+      # find masses from label
+      mutate(
+        numerator_mass = str_match(Label, "^(\\d+)/") %>% {.[,2]},
+        denominator_mass = str_match(Label, "/(\\d+)$") %>% {.[,2]}
+      ) %>% 
+      # channel to mass matches
+      {
+        bind_rows(
+          select(., channel=NumeratorBeamChannel, mass=numerator_mass),
+          select(., channel=DenominatorBeamChannel, mass=denominator_mass)
+        )
+      } %>% 
+      unique()
+    return(channel_defs)
+  }
+  
+  # find the H3 factor
+  xml_find_H3_factor <- function(node) {
+    H3_factor <- 
+      node %>% xml_child("SerialisedPropertyBagProperties") %>%
+      xml_fetch_container_value(c("ApplyH3CorrectionFactor", "H3CorrectionFactor"))
+    if (!is.na(H3_factor$ApplyH3CorrectionFactor) && H3_factor$ApplyH3CorrectionFactor == "True") 
+      return(as.numeric(H3_factor$H3CorrectionFactor))
+    else return(NULL)
+  }
+  
+  # process channel configurations
+  species_config <- xml %>% 
+    xml_find_all("//SerialisablePropertyBag[Identifier[.='10DC1602-5ED4-4D62-BAB0-2693E3FBC3AF']]") %>% 
+    sapply(function(node) {
+      species <- xml_find_species(node)
+      if (is.null(species) || is.na(species)) # no species definition found
+        return(list())
+      
+      config <- list(channels = xml_find_channel_masses(node))
+      if (!is.null(H3_factor <- xml_find_H3_factor(node))) config$H3_factor <- H3_factor
+      
+      config %>% list() %>% setNames(species)
+    })
+  
+  # info
+  if (!quiet) {
+    sprintf("      found configurations for '%s'", 
+            species_config %>% names() %>% str_c(collapse = "', '")) %>% 
+      message()
+  }
+  
+  # debug
+  if (default("debug")) {
+    message("DEBUG: species configurations: ")
+    print(species_config)
+  }
+  
+  list(list(species = species_config))
+}
+
