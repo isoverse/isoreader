e4fc541f07bc109f5c1cac3c9c78a0bed47fe209
Sebastian Kopf <seb.kopf@gmail.com>
2017-07-23 18:26:36 -0600

    rename isoexport_ to just export_

--- R/export.R
+++ R/export.R
@@ -1,21 +1,23 @@
 ## Export functions
 
-#' Export data to R Data Archive
+#' Export data to R Data Archive (.rda)
 #' 
-#' @inheritParams isoplot_raw_data
+#' This function exports the passed in isofiles to an R Data Archive (.rda) file, which is a fairly efficient compressed data storage format. Data exported this way can be easily read back into isoreader using the standard \code{\link{read_continuous_flow}} and \code{\link{read_dual_inlet}} functions or by simply double-clicking the .rda file which will open in an R console and restore the data into variable \code{isofiles}.
+#' 
+#' @inheritParams aggregate_raw_data
 #' @param filename the name of the file to export (without the Rda ending)
-#' @param folder the folder where to save the Rda file
+#' @param folder the folder where to save the rda file
 #' @family export functions
 #' @return returns the isofiles object invisibly for use in pipelines
 #' @export
-isoexport_rda <- function(isofiles, filename, folder = ".", quiet = setting("quiet")) {
+export_to_rda <- function(isofiles, filename, folder = ".", quiet = setting("quiet")) {
   
   # safety checks
   if(!is_iso_object(isofiles)) stop("can only export iso files or lists of iso files", call. = FALSE)
   else if (is_continuous_flow(isofiles))
-    ext <- ".cf.Rda"
+    ext <- ".cf.rda"
   else if (is_dual_inlet(isofiles))
-    ext <- ".di.Rda"
+    ext <- ".di.rda"
   else
     stop("R data archive export of this type of isofiles not supported", call. = FALSE) 
   if (missing(filename)) stop("no filename provided", call. = FALSE)
