f40dfcac89f0827b980165c06fb7328d3b1a9a2d
Sebastian Kopf <seb.kopf@gmail.com>
2017-08-16 15:34:18 +0200

    switch from filename/folder to filepath structure

--- R/export.R
+++ R/export.R
@@ -5,12 +5,11 @@
 #' This function exports the passed in isofiles to an R Data Archive (.rda) file, which is a fairly efficient compressed data storage format. Data exported this way can be easily read back into isoreader using the standard \code{\link{read_continuous_flow}} and \code{\link{read_dual_inlet}} functions or by simply double-clicking the .rda file which will open in an R console and restore the data into variable \code{isofiles}.
 #' 
 #' @inheritParams aggregate_raw_data
-#' @param filename the name of the file to export (without the Rda ending)
-#' @param folder the folder where to save the rda file
+#' @param filepath the path (folder and filename) to the export file. The correct file extension is automatically added if not already in the filename, i.e. filename can be provided with or without extension.
 #' @family export functions
 #' @return returns the isofiles object invisibly for use in pipelines
 #' @export
-export_to_rda <- function(isofiles, filename, folder = ".", quiet = setting("quiet")) {
+export_to_rda <- function(isofiles, filepath, quiet = setting("quiet")) {
   
   # safety checks
   if(!is_iso_object(isofiles)) stop("can only export iso files or lists of iso files", call. = FALSE)
@@ -20,8 +19,12 @@ export_to_rda <- function(isofiles, filename, folder = ".", quiet = setting("qui
     ext <- ".di.rda"
   else
     stop("R data archive export of this type of isofiles not supported", call. = FALSE) 
-  if (missing(filename)) stop("no filename provided", call. = FALSE)
-  if (!file.exists(folder)) stop("the provided folder '", folder, "' does not exist", call. = FALSE)
+  
+  # file name and folder
+  if (missing(filepath)) stop("no filepath provided", call. = FALSE)
+  filename <- basename(filepath)
+  folder <- dirname(filepath)
+  if (!file.exists(folder)) stop("the folder '", folder, "' does not exist", call. = FALSE)
   
   # save isofiles
   isofiles <- as_isofile_list(isofiles)
@@ -31,4 +34,19 @@ export_to_rda <- function(isofiles, filename, folder = ".", quiet = setting("qui
     sprintf("Info: exporting data from %d files into '%s'", length(isofiles), str_replace(filepath, "^\\.(/|\\\\)", "")) %>% message()
   save(isofiles, file = filepath)
   return(invisible(isofiles))
+}
+
+
+#' Export data to Excel
+#' 
+#' Convenience function for exporting isoreader data to Excel.
+#' 
+#' @inheritParams export_to_rda
+#' @param filename the name of the file to export (without the Rda ending)
+#' @param folder the folder where to save the rda file
+#' @family export functions
+#' @return returns the isofiles object invisibly for use in pipelines
+#' @export
+export_to_excel <- function(isofiles, filenname, folder = ".", quiet = setting("quiet")) {
+  
 }
--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -2,6 +2,7 @@ context("Export functions")
 
 test_that("test that export to rda works properly", {
   expect_error(export_to_rda(42), "can only export iso files")
-  expect_error(export_to_rda(isoreader:::make_cf_data_structure()), "no filename provided")
-  expect_error(export_to_rda(isoreader:::make_cf_data_structure(), "test", folder = "DOESNOTEXIST"), "provided folder .* does not exist")
+  expect_error(export_to_rda(isoreader:::make_cf_data_structure()), "no filepath provided")
+  expect_error(export_to_rda(isoreader:::make_cf_data_structure(), file.path("DOESNOTEXIST", "test")), 
+               "folder .* does not exist")
 })
