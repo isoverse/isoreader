eb31d23e08d62f545edc5d5a8c659114894e79f8
Sebastian Kopf <seb.kopf@gmail.com>
2018-03-02 11:42:24 -0700

    use get_expr instead of UQE

--- R/package.R
+++ R/package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 #' @import tidyr
-#' @importFrom rlang enquo quo quos UQ UQE !! !!! quo_expr quo_text quo_is_null quo_is_symbol quo_is_lang is_quosure is_empty is_integerish eval_tidy sym lang_head
+#' @importFrom rlang enquo quo quos UQ !! !!! get_expr quo_expr quo_text quo_is_null quo_is_symbol quo_is_lang is_quosure is_empty is_integerish eval_tidy sym lang_head
 #' @importFrom tidyselect everything starts_with ends_with matches vars_select
 #' @importFrom dplyr vars n select select_ rename rename_ arrange desc mutate mutate_ mutate_at filter filter_ distinct as_data_frame left_join right_join full_join data_frame bind_rows bind_cols group_by group_by_ ungroup tally summarize do
 #' @importFrom glue glue collapse
