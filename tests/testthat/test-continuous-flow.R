db1c8a86a0f2e99e6042904b2b13b4f8f366845b
Ilja Kocken <i.j.kocken@uu.nl>
2018-11-15 14:42:49 +0100

    Revert "fix testthat call"
    This reverts commit 2b962cb89269207ba570910d252883a0b9df4fe3.
    
    I forgot to turn off the whitespace rule in my editor

--- tests/testthat/test-continuous-flow.R
+++ tests/testthat/test-continuous-flow.R
@@ -3,30 +3,30 @@ context("Continuous flow")
 test_that("test that supported cf files are correct", {
   isoreader:::initialize_options()
   expect_is(exts <- isoreader:::get_supported_cf_files(), "data.frame")
-  expect_equal(exts$extension, c(".cf", ".dxf", ".iarc", ".cf.rda", ".cf.rds"))
+  expect_equal(exts$extension, c(".cf", ".dxf", ".iarc", ".cf.rds", ".cf.rda"))
   expect_true(all(exts$func %>% sapply(class) == "character"))
   expect_true(all(exts$func %>% map_lgl(exists, mode = "function", where = asNamespace("isoreader"))))
 })
 
 test_that("test that parameter checks are performed", {
-
+  
   # flow iarc
-  expect_error(isoreader:::iso_read_flow_iarc (isoreader:::make_di_data_structure()),
+  expect_error(isoreader:::iso_read_flow_iarc (isoreader:::make_di_data_structure()), 
                "data structure must be a \\'continuous_flow\\' iso_file")
-
-
+  
+  
 })
 
 
 test_that("test that dxf files can be read", {
   # test specific files
-
+  
   # FIXME: re-enable for commits
   # skip("Currently not testing all continuous flow data files.")
   # FIXME: run as one batch to make use of parallel processing
-
+  
   iso_turn_reader_caching_off()
-
+  
   expect_true(file.exists(file <- iso_get_reader_example("linearity_example.dxf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
@@ -34,47 +34,47 @@ test_that("test that dxf files can be read", {
   expect_true(file.exists(file <- iso_get_reader_example("continuous_flow_example.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- iso_get_reader_example("continuous_flow_example.dxf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "dxf_example_H_01.dxf")))
   expect_is(dxf1 <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf1)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "dxf_example_HO_01.dxf")))
   expect_is(dxf2 <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf2)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_01.cf")))
   expect_is(dxf3 <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf3)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_02.cf")))
   expect_is(dxf4 <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf4)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_03.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_04.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_05.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_06.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "cf_example_H_07.cf")))
   expect_is(dxf <- iso_read_continuous_flow(file), "continuous_flow")
   expect_equal(nrow(problems(dxf)), 0)
-
+  
   # test re-reading
   # NOTE: ideally this should also include an iarc file
   iso_files <- c(dxf1, dxf2, dxf3)
@@ -84,6 +84,7 @@ test_that("test that dxf files can be read", {
   )
   expect_true(iso_is_continuous_flow(reread_dxf <- iso_reread_files(iso_files)))
   expect_equal(nrow(problems(reread_dxf)), 0)
-
-
+  
+  
 })
+
--- tests/testthat/test-dual-inlet.R
+++ tests/testthat/test-dual-inlet.R
@@ -3,54 +3,54 @@ context("Dual Inlet Files")
 test_that("test that supported di files are correct", {
   isoreader:::initialize_options()
   expect_is(exts <- isoreader:::get_supported_di_files(), "data.frame")
-  expect_equal(exts$extension, c(".did", ".caf", ".di.rda", ".di.rds"))
+  expect_equal(exts$extension, c(".did", ".caf", ".di.rds", ".di.rda"))
   expect_true(all(exts$func %>% sapply(class) == "character"))
   expect_true(all(exts$func %>% map_lgl(exists, mode = "function", where = asNamespace("isoreader"))))
 })
 
 test_that("test that parameter checks are performed", {
 
-  expect_error(isoreader:::iso_read_did(isoreader:::make_cf_data_structure()),
+  expect_error(isoreader:::iso_read_did(isoreader:::make_cf_data_structure()), 
                "data structure must be a \\'dual_inlet\\' iso_file")
-
+  
 })
 
 test_that("test that did files can be read", {
   # test specific files
-
+  
   # FIXME: re-enable for commits
   # skip("Currently not testing all dual inlet data files.")
   # FIXME: run as one batch to make use of parallel processing
-
+  
   iso_turn_reader_caching_off()
-
+  
   # .did files
   expect_true(file.exists(file <- iso_get_reader_example("dual_inlet_example.did")))
   expect_is(did <- iso_read_dual_inlet(file), "dual_inlet")
   expect_equal(nrow(problems(did)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "did_example_CO2_clumped_01.did")))
   expect_is(did <- iso_read_dual_inlet(file), "dual_inlet")
   expect_equal(nrow(problems(did)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "did_example_many_cycles.did")))
   expect_is(did <- iso_read_dual_inlet(file), "dual_inlet")
   expect_equal(nrow(problems(did)), 0)
-
+  
   expect_true(file.exists(file <- file.path("test_data", "did_example_unicode.did")))
   expect_is(did <- iso_read_dual_inlet(file), "dual_inlet")
   expect_equal(nrow(problems(did)), 0)
-
+  
   # minimal files
   expect_true(dir.exists(files <- file.path("test_data", "minimal_files")))
   expect_true(iso_is_file_list(dids <- iso_read_dual_inlet(files)))
-  expect_true(dids %>% iso_get_file_info(select = c(Comment, starts_with("MS"))) %>%
-                mutate(MSIT_correct = Comment == paste(MS_integration_time.s, "sec")) %>%
+  expect_true(dids %>% iso_get_file_info(select = c(Comment, starts_with("MS"))) %>% 
+                mutate(MSIT_correct = Comment == paste(MS_integration_time.s, "sec")) %>% 
                 { all(.$MSIT_correct) })
-
+  
   # .caf files
   expect_true(file.exists(file <- file.path("test_data", "caf_example_CO2_01.caf")))
   expect_is(did <- iso_read_dual_inlet(file), "dual_inlet")
   expect_equal(nrow(problems(did)), 0)
-
+  
 })
