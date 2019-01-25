ca9a9f197ef8425ef534c785a4f180ed9666f1ba
Sebastian Kopf <sebastian.kopf@colorado.edu>
2019-01-25 03:39:30 -0700

    migrate ratio calculator

--- tests/testthat/test-plotting.R
+++
@@ -1,7 +0,0 @@
-context("Plotting functions")
-
-test_that("test that plottings functions throw deprecation error", {
-  expect_error(iso_plot_raw_data(), "moved to the isoprocessor")
-  expect_error(iso_plot_dual_inlet_data(), "moved to the isoprocessor")
-  expect_error(iso_plot_continuous_flow_data(), "moved to the isoprocessor")
-})
--- tests/testthat/test-unit-conversion.R
+++
@@ -1,6 +0,0 @@
-context("Unit scaling")
-
-test_that("test that plottings functions throw deprecation error", {
-  expect_error(iso_convert_time(), "moved to the isoprocessor")
-  expect_error(iso_convert_signals(), "moved to the isoprocessor")
-})
