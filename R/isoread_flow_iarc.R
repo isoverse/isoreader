e006a8acb2b17aa8ee4f96246be4c87a45eef23d
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-02-09 16:49:17 -0700

    make tp in iarc files an integer (as it always is)

--- R/isoread_flow_iarc.R
+++ R/isoread_flow_iarc.R
@@ -225,7 +225,7 @@ read_irms_data_file <- function(iso_file, filepath, gas_config, run_time.s, data
   dt <- run_time.s / nrow(irms_data)
   irms_data <- irms_data %>% 
     rename(tp = Scan) %>% 
-    mutate(time.s = dt * tp) %>% 
+    mutate(tp = as.integer(tp), time.s = dt * tp) %>% 
     select(tp, time.s, everything())
   
   # store mass data
