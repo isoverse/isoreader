17e01232c56720f0bef9275f9209d461be9dfed0
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-02-17 16:32:56 -0700

    fix windows reading problem in binary files

--- R/utils_binary_files.R
+++ R/utils_binary_files.R
@@ -287,9 +287,17 @@ re_combine <- function(...) {
   structure(
     list(
       label = str_c(map_chr(regexps, "label"), collapse = ""),
-      # NOTE: on windows, the following command with str_c instead of paste or map_chr instead of sapply strangly leads to the regexp not getting recognized anymore in grepRaw
-      regexp = stringr::str_c(sapply(regexps, `[[`, "regexp"), collapse = ""),
-      size = sum(map_dbl(regexps, "size"))
+      size = sum(map_dbl(regexps, "size")),
+      # WARNING WARNING WARNING
+      # do not change this
+      # on all platforms: map_chr messes up the regexps
+      # on windows: str_c instead of paste leads to regexp fail in grepRaw
+      # on unix: paste instead of str_c breaks concatenating a few specific regexps (e.g. re_combine(re_not_null(2), re_block("fef-x")))
+      regexp = 
+        if (.Platform$OS.type == "windows") 
+          paste(sapply(regexps, `[[`, "regexp"), collapse = "")
+        else
+          stringr::str_c(sapply(regexps, `[[`, "regexp"), collapse = "")
     ),
     class = "binary_regexp")
 }
