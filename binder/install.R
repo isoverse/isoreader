4d8e7f32e63cded4ab546b3aa9315b5c0d6e391d
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-11-11 10:40:21 -0700

    add readxl

--- binder/install.R
+++ binder/install.R
@@ -5,5 +5,8 @@ BiocManager::install("rhdf5")
 # Package itself
 install.packages("isoreader")
 
+# Additional packages (in suggests for isoreader 1.2.7)
+install.packages("readxl")
+
 # Packages for knitting
 install.packages(c("rmarkdown", "caTools", "bitops"))
