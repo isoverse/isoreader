13600cbaa29efbfd9b26fb1b6b8979de37e5f841
Sebastian Kopf <sebastian.kopf@colorado.edu>
2023-07-27 17:08:34 -0600

    update deprecated tests

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -2,7 +2,7 @@ context("Deprecated functions")
 
 test_that("test that deprecated functions give the appropriate error", {
   
-  expect_warning(iso_export_to_excel(), "deprecated")
-  expect_warning(iso_export_to_feather(), "deprecated")
+  expect_warning(tryCatch(iso_export_to_excel(), error = function(e){}), "deprecated")
+  expect_warning(tryCatch(iso_export_to_feather(), error = function(e){}), "deprecated")
   
 })
