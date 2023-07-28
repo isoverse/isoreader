4b9a19fdf209053b1b5fa48ad4bc9880a2ae4db4
Sebastian Kopf <sebastian.kopf@colorado.edu>
2023-07-27 18:03:36 -0600

    check for deprecated

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -2,6 +2,10 @@ context("Deprecated functions")
 
 test_that("test that deprecated functions give the appropriate error", {
   
+  expect_warning(tryCatch(iso_get_data(), error = function(e){}), "deprecated")
+  expect_warning(tryCatch(iso_get_standards_info(), error = function(e){}), "deprecated")
+  expect_warning(tryCatch(iso_get_resistors_info(), error = function(e){}), "deprecated")
+  expect_warning(tryCatch(iso_omit_files_with_problems(), error = function(e){}), "deprecated")
   expect_warning(tryCatch(iso_export_to_excel(), error = function(e){}), "deprecated")
   expect_warning(tryCatch(iso_export_to_feather(), error = function(e){}), "deprecated")
   
